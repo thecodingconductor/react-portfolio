949c21e4b7626a6929537bdc843192727d969da2
thecodingconductor <tristan.raissherman@gmail.com>
2020-11-19 15:27:51 -0500

    getting ready for deployment

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -8,10 +8,13 @@ import tristan from '../images/Tristan_Rais_Sherman_0036_web_DO_NOT_PRINT.jpg'
 const Home = () => {
     return (
         <Container className="website-title-container">
-            <h1 className="website-title">
-                Tristan is a front-end developer and musician.
-            </h1>
-            <img src={tristan} alt="Tristan Headshot" className="tristan-headshot" />
+            <div className="d-flex align-items-center top-container">
+                <h1 className="website-title">
+                    Tristan is a front-end developer and musician.
+                </h1>
+                <img src={tristan} alt="Tristan Headshot" className="tristan-headshot" />
+            </div>
+
             <Container className="projects-showcase-container">
                 <ProjectsShowcase />
             </Container>
