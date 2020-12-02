1e17c9a34044c1d18cef389f9d88cbdadfde3c35
thecodingconductor <tristan.raissherman@gmail.com>
2020-12-02 09:42:28 -0500

    removed picture, added margin between projects

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 import { Container } from 'react-bootstrap';
 
-import tristan from '../images/Tristan_Rais_Sherman_0036_web_DO_NOT_PRINT.jpg'
+// import tristan from '../images/Tristan_Rais_Sherman_0036_web_DO_NOT_PRINT.jpg';
+import tristan from '../images/tristanSitting.jpg'
 import ConcertBuilderShowcase from '../projects/ConcertBuilderShowcase';
 import OrchesNationShowcase from '../projects/OrchesNationShowcase';
 import MarsRoverShowcase from '../projects/MarsRoverShowcase';
@@ -13,11 +14,11 @@ const Home = () => {
 
     return (
         <Container className="website-title-container">
-            <div className="d-flex align-items-center top-container">
+            <div className="d-flex align-items-center justify-content-center top-container">
                 <h1 className="website-title">
                     Tristan is a front-end developer and musician.
                 </h1>
-                <img src={tristan} alt="Tristan Headshot" className="tristan-headshot" />
+                {/* <img src={tristan} alt="Tristan Headshot" className="tristan-headshot" /> */}
             </div>
 
             <Container className="projects-showcase-container">
